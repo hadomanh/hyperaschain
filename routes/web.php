8d97f2b374de4d25ce968094ba9af72abbbb15ee
chinhdtth2009021 <chinhdtth2009021@fpt.edu.vn>
2022-05-09 10:41:22 +0700

    chinh

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/', function () {
 Route::get('/about-us', function () {
     return view('about-us');
 })->name('about-us');
-Route::get('/pagination/community', function () {
-    return view('pagination/community');
-})->name('pagination/community');
+Route::get('/community', function () {
+    return view('community');
+})->name('community');
 Auth::routes();
 
 Route::middleware('auth')->prefix('admin')->group(function () {
