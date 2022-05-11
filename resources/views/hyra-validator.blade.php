10933def6c56e94bf40c32db79c0c9cdae4b684f
mannghi <mannghi@gmail.com>
2022-05-11 16:11:48 +0700

    hyra validator

--- resources/views/hyra-validator.blade.php
+++ resources/views/hyra-validator.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 
 <div id="hyraValidater">
+    <section class="banner text-center">
+        <h1 class="mb-0">HYPERAS VALIDATOR</h1>
+    </section>
     <section>
         <div class="container">
-            <h1 class="text-center pt-3 fw-bold">HYPERAS VALIDATOR</h1>
             <div class="row d-flex justify-content-center">
                 <div class="col-md-4 bgNumber">
                     <div class="number">
@@ -17,14 +19,14 @@
                     <strong>Current number of validators on mainnet</strong>
                 </div>
                 <div class="col-md-4 bgNumber">
-                    <div class="number">
-                        <div class="sectionHeading numberR">3 2</div>
+                    <div class="">
+                        <div class="sectionHeading number">3 2</div>
                     </div>
                     <strong>Current number of validantors on mainnet</strong>
                 </div>
                 <div class="col-5 bgleft">
                     <div class="bgDocs">
-                        <h1 class="fw-bold">Get Started</h1>
+                        <h2 class="fw-bold">Get Started</h2>
                         <p>Go here for documentation related to all things Validator, from validator requirements, to troubleshooting.</p>
                         <div class="text-end docText"><a href="#">Go to docs</a> <i class="fas fa-angle-right"></i></div>
                     </div>
@@ -32,7 +34,7 @@
                 </div>
                 <div class="col-5 bgright">
                     <div class="bgDocs">
-                        <h1 class="fw-bold">Validator Explorer</h1>
+                        <h2 class="fw-bold">Validator Explorer</h2>
                         <p>View the current list of validators, their ranking by stake amount, and information about commission below.</p>
                         <div class="text-end docText"><a href="#">Go to docs</a> <i class="fas fa-angle-right"></i></div>
                     </div>
@@ -41,7 +43,7 @@
             </div>
             <div class="text-center"><img src="{{ asset('img/square.png') }}" alt="" class="imgSquare"></div>
             <div class="text-center">
-                <h1 class="sectionHeading numberR">What is Validator with PoA-based networks?</h1>
+                <h2 class="sectionHeading">What is Validator with PoA-based networks?</h2>
                 <p class="pt-2 pb-4">In PoA-based networks, transactions and blocks are validated by approved accounts, known as validators. Validators run software allowing them to put transactions in blocks. The process is automated and does not require validators to be constantly monitoring their computers. It, however, does require maintaining the computer (the authority node) uncompromised.</p>
                 <div class="button-box-submit">
                     <a type="button" class="btn">Become Validator</a>
