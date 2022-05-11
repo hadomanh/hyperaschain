584fa1c24633a5443842aaccc4b6c89549b00ded
vudinhkhoi <zluffyz2002@gmail.com>
2022-05-11 17:11:46 +0700

    fix css

--- resources/views/guide-line.blade.php
+++ resources/views/guide-line.blade.php
@@ -1,82 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="guildline-content-top">
-    <div class="container">
-        <h1 class="text-center text-light">
+<div id="guideLinePage"  class="text-center">
+    <section class="banner">
+        <h1 class=" text-light">
             GUILDLINE
         </h1>
-        <p class="text-center content-text-guildline text-light">
+        <p class=" content-text-guildline text-light">
             A decentralized exchange where cryptocurrencies and other digital assets (NFTs) are listed. Hyra Dex is considered a great step <br> forward in the mission of currency liberalization, decentralized crypto-assets of the Hyperas Chain ecosystem...
         </p>
-        <div class="row d-flex">
-            <div class="col-3 text-center box-step">
-                <img class="image-guildline" src="{{ asset('img/Framee.png') }}" alt="">
-                <h3 class="text-light progress-after position-relative">
-                    Step 1
-                </h3>
-                <p class="text-light">
-                    Fill out the registration form to send us information.
-                </p>
-            </div>
-            <div class="col-3 text-center box-step">
-                <img class="image-guildline" src="{{ asset('img/Framee1.png') }}" alt="">
-                <h3 class="text-light progress-after position-relative">
-                    Step 2
-                </h3>
-                <p class="text-light">
-                    Hyperas conducts internal   of project information before proceeding to digitize assets.
-                </p>
-            </div>
-            <div class="col-3 text-center box-step">
-                <img class="image-guildline" src="{{ asset('img/New_Symbol_4_copy.png') }}" alt="">
-                <h3 class="text-light progress-after position-relative">
-                    Step 3
-                </h3>
-                <p class="text-light">
-                    Digitize assets through Hyperas Chain
-                </p>
-            </div>
-            <div class="col-3 text-center box-step">
-                <img class="image-guildline image-guildline-4" src="{{ asset('img/Framee3.png') }}" alt="">
-                <h3 class="text-light position-relative">
-                    Step 4
-                </h3>
-                <p class="text-light">
-                    Publish digital assets to appropriate platforms depending on the needs of the project.
-                </p>
+    </section>
+    <section class="guildline-content-top text-center">
+        <div class="container">
+            <div class="row d-flex">
+                <div class="col-3">
+                    <div class="roundPink">
+                        <img src="{{ asset('img/Framee.png') }}" alt="">
+                    </div>
+                    <h3 class="text-light progress-after position-relative">
+                        Step 1
+                    </h3>
+                    <p class="text-light">
+                        Fill out the registration form to send us information.
+                    </p>
+                </div>
+                <div class="col-3 ">   
+                    <div class="roundPink">
+                        <img src="{{ asset('img/Framee1.png') }}" alt="">
+                    </div>
+                    <h3 class="text-light progress-after position-relative">
+                        Step 2
+                    </h3>
+                    <p class="text-light">
+                        Hyperas conducts internal   of project information before proceeding to digitize assets.
+                    </p>
+                </div>
+                <div class="col-3">
+                    <div class="roundPink">
+                        <img src="{{ asset('img/New_Symbol_4_copy.png') }}" alt="">
+                    </div>
+                    <h3 class="text-light progress-after position-relative">
+                        Step 3
+                    </h3>
+                    <p class="text-light">
+                        Digitize assets through Hyperas Chain
+                    </p>
+                </div>
+                <div class="col-3">
+                   
+                    <div class="roundPink">
+                        <img src="{{ asset('img/Framee3.png') }}" alt="">
+                    </div>
+                    <h3 class="text-light position-relative">
+                        Step 4
+                    </h3>
+                    <p class="text-light">
+                        Publish digital assets to appropriate platforms depending on the needs of the project.
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="box-contact-hyperras ">
-    <div class="container position-relative">
-        <div class="bg-contact-hyperras text-center">
-            <img src="{{ asset('img/bg-contact-galaxy.png') }}" alt="">
-        </div>
-        <div class="position-absolute content-bg-contacthyperas">
-            <p class="content-text-hyperras">
-                Contact Hyperas
-            </p>
-            <div>
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Your Name*</label>
+    </section>
+    <section class="box-contact-hyperras ">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+
                 </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Your Email*</label>
-                </div>
-                <div class="form-floating box-message">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Message*</label>
-                </div>
-                <div class="button-box-submit">
-                    <button type="button" class="btn btn-primary">SUBMIT</button>
+                <div class=" col-6 content-bg-contacthyperas">
+                    <p class="content-text-hyperras">
+                        Contact Hyperas
+                    </p>
+                        <div class="mb-3">
+                            <input type="text" class="form-control form-control-lg" placeholder="Your Name*">
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control form-control-lg" placeholder="Your Email*">
+                        </div>
+                        <div class="mb-3">
+                            <input class="form-control form-control-lg "  placeholder ="Message*" rows="3"></input>
+                        </div>
+                        <div class="button-box-submit">
+                            <button type="button" class="btn btn-primary"><a href="">SUBMIT</a></button>
+                        </div>
                 </div>
             </div>
-            
         </div>
-    </div>
-</section>
+    </section>
+</div>
 @endsection
--- resources/views/pagination/hyra-staking-2.blade.php
+++ resources/views/pagination/hyra-staking-2.blade.php
@@ -1,33 +1,127 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hyra-staking-page-1">
-        <h1 class="text-center">
-            Getting Started HYRA Staking 
-        </h1>
-    <div class="">   
-        <div class="row">
-            <div class="col-4">
-                
-            </div>
-            <div class="col-4">
+<div id="hyra-staking-page-1">
+    <section>
+        <div class="container">
+            <h1 class="text-center">
+                Getting Started HYRA Staking 
+            </h1>
+            <div class="">   
+                <div class="row">
+                    <div class="col-4 border-box">
+                        <div class="d-flex image-hyra-staking">
+                            <img src="{{ asset('img/Ellipse 18.png')}}" alt="">
+                            <div class="distance-stake-hyra-top">
+                                <span>
+                                    Stake HYRA
+                                </span>
+                                <p class="text-stake-hyra-top">
+                                    Stake, Earn - And more!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="d-flex box-content-top-1">
+                            <p class="text-hyra-box">
+                                FLEXIBLE APY:
+                            </p>
+                            <h4 class="text-number">
+                                17.38%
+                            </h4>
+                        </div>
+                        <div class="d-flex box-content-top-2">
+                            <p class="text-hyra-box">
+                                LOCKED APY:
+                            </p>
+                            <h4 class="text-number">
+                                Up to 17.38%
+                            </h4>
+                        </div>
+                        <p class="text-start text-center">
+                            Start Staking
+                        </p>
+                        <div>
+                            <h5 class="text-center border-box-connect">
+                                <a href="Connect Wallet">Connect Wallet</a>
+                            </h5>
+                            <hr class="horizontal-hyra">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="d-flex image-hyra-staking">
+                            <img src="{{ asset('img/Ellipse 18.png')}}" alt="">
+                                <div class="distance-stake-hyra-top">
+                                    <span>
+                                        Earn HYRA
+                                    </span>
+                                    <p class="text-stake-hyra-top">
+                                        Stake HYRA
+                                    </p>
+                                </div>
+                        </div>
+                        <div class="d-flex box-content-top-1">
+                            <p class="text-hyra-box">
+                                APR:
+                            </p>
+                            <h4 class="text-number">
+                                Up to 17.38%
+                            </h4>
+                        </div>
+                        <p class="text-start text-center">
+                            Start Staking
+                        </p>
+                        <div>
+                            <h5 class="text-center border-box-connect">
+                                <a href="Connect Wallet">Connect Wallet</a>
+                            </h5>
+                            <hr class="horizontal-hyra">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="d-flex image-hyra-staking">
+                            <img src="{{ asset('img/Ellipse 19.png  ')}}" alt="">
+                                <div class="distance-stake-hyra-top">
+                                    <span>
+                                        Earn HYRA
+                                    </span>
+                                    <p class="text-stake-hyra-top">
+                                        Stake HYRA
+                                    </p>
+                                </div>
+                        </div>
+                        <div class="d-flex box-content-top-1">
+                            <p class="text-hyra-box">
+                                APR:
+                            </p>
+                            <h4 class="text-number">
+                                Up to 17.38%
+                            </h4>
+                        </div>
+                        <p class="text-start text-center">
+                            Start Staking
+                        </p>
+                        <div class="box-wallet">
+                            <h5 class="text-center border-box-connect">
+                               <a href="Connect Wallet">Connect Wallet</a>
+                            </h5>
+                        </div>
+                            <hr class="horizontal-hyra">
+                    </div>
+                    
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        
+                    </div>
+                    <div class="col-4">
 
-            </div>
-            <div class="col-4">
+                    </div>
+                    <div class="col-4">
 
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-4">
-
-            </div>
-            <div class="col-4">
-
-            </div>
-            <div class="col-4">
-
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
+</div>
 @endsection
