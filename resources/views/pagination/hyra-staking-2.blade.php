0fdec81970a4e94475609d488adb7bbf0fa4d970
vudinhkhoi <zluffyz2002@gmail.com>
2022-05-11 10:15:49 +0700

    hyra-staking-2

--- resources/views/pagination/hyra-staking-2.blade.php
+++ resources/views/pagination/hyra-staking-2.blade.php
@@ -2,10 +2,32 @@
 
 @section('content')
 <section class="hyra-staking-page-1">
-    <div>
         <h1 class="text-center">
             Getting Started HYRA Staking 
         </h1>
+    <div class="">   
+        <div class="row">
+            <div class="col-4">
+                
+            </div>
+            <div class="col-4">
+
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+
+            </div>
+            <div class="col-4">
+
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
     </div>
 </section>
 @endsection
