c37e50ec0f479994976f547eb9052b5784fd2691
vudinhkhoi <zluffyz2002@gmail.com>
2022-05-11 10:01:45 +0700

    page staking

---
+++ resources/views/pagination/hyra-staking-2.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="hyra-staking-page-1">
+    <div>
+        <h1 class="text-center">
+            Getting Started HYRA Staking 
+        </h1>
+    </div>
+</section>
+@endsection
